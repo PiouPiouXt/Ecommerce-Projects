a027303b582fa1be83dad8671d76e2f40cc354bf
Piou <tsiresyrazak19@gmail.com>
2026-07-27 20:39:33 +0300

    fetch from the backend

--- e-commerce/src/pages/HomePage.jsx
+++ e-commerce/src/pages/HomePage.jsx
@@ -5,10 +5,10 @@ import './HomePage.css'
 export function HomePage() {
   fetch('http://localhost:3000/api/products')
     .then((response) => {
-      response.json().then((data) => {
+      return response.json()
+    }).then((data) => {
         console.log(data);
       });
-    })
 
   return (
     <>
