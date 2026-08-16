f7bd2f9e96d1a83e69e9e352b5727405a5b61b35
Piou <tsiresyrazak19@gmail.com>
2026-08-16 14:03:37 +0300

    starting automated test course

---
+++ e-commerce/src/pages/home/Product.test.jsx
@@ -0,0 +1,20 @@
+// import { it, expect, describe } from 'vitest';
+// import { render } from '@testing-library/react';
+// import { Product } from './Product';
+
+// describe('Product component', () => {
+//   it('display the product details correctly',() => {
+//     const product = {
+//     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87
+//     },
+//     priceCents: 1090,
+//     keywords: ["socks", "sports", "apparel"]
+//   }
+//     render(<Product product={product}/>)
+//   });  
+// })
